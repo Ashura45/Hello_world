9a8ec1af2a3885f34c2aa2265cb39ecb2709bb68
Алеша <144483223+Ashura45@users.noreply.github.com>
2024-12-05 22:53:53 +0500

    Hello world!.py

--- Hello world!.py
+++ Hello world!.py
@@ -3,6 +3,8 @@ class Hello_world:
     def say(self):
         return self.text
 
-hello = Hello_world()
-print(hello.say())
+
+if __name__ == "__main__":
+    hello = Hello_world()
+    print(hello.say())
 
